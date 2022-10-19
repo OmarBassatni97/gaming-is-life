9de3991523b4d06e370e0c1de079e390c4fc0066
Omar Bassatni <omarbassatni@gmail.com>
2022-10-19 20:13:21 +0300

    changed app title

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import { Routes, Route } from 'react-router-dom'
 import SpecificGame from "./pages/SpecificGame";
 import { GameProvider } from "./store/GameStore";
 import MoreDetails from "./pages/MoreDetails";
+import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <GameProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Gaming is Life</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Gaming App" />
+      </Helmet>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
